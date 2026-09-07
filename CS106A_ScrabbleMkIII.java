97c22f8f9f32190c7754a62636f399cf69e8d89a
Noah Pope <npope@tutanota.com>
2026-09-07 16:20:59 -0500

    adding files to new repo

---
+++ CS106A_ScrabbleMkIII.java
@@ -0,0 +1,73 @@
+//CS106A_ScrabbleMkIII
+/**
+ * File: CS106A_ScrabbleMkIII.java
+ * --------------------------------------
+ * The CS106A_ScrabbleMkIII class mimics the game
+ * known as Scrabble, where each capital letter 
+ * in the English alphabet is awarded a point 
+ * inversely related to its frequency in English 
+ * words.
+ * 
+ */
+
+import acm.program.*;
+import java.util.*;
+
+public class CS106A_ScrabbleMkIII extends ConsoleProgram
+{
+	/** Immutable Instance Variables (named constants) */
+	
+	public void run()
+	{
+		//make an instance var to keep track of the points
+//		make an enum of the alphabet w a val pt associated w
+		//..each letter (see pg 286 in art & sci text)
+		//
+		introduceApp();
+		askClientForInput();
+	}
+	
+	
+	private void introduceApp()
+	{
+		println("The CS106A_ScrabbleMkIII class mimics the game" +
+				"known as Scrabble, where each capital letter " +
+				"in the English alphabet is awarded a point " +
+				"inversely related to its frequency in English words.");
+		pause(2000);
+	}
+	
+	
+	private void askClientForInput()
+	{
+		String clientInput = readLine("Enter a word. No spaces or numbers: ");
+		if (checkClientInputValidityFor(clientInput)){
+			handlePointsFor(clientInput);
+		} else { 
+			println("Invalid input. Please trya again, omitting spaces and numbers.");
+			askClientForInput();
+		}
+	}
+	
+	
+	private boolean checkClientInputValidityFor(String word)
+	{
+		StringTokenizer tokenizedString = new StringTokenizer(word);
+		while (tokenizedString.hasMoreTokens()){
+			String token = tokenizedString.nextToken();
+			if (!token.equals(' ')){
+				
+			}
+		}
+		return true;
+	}
+	
+	
+	private void handlePointsFor(String word)
+	{
+		StringTokenizer tokenizedString = new StringTokenizer(word);
+	}
+	
+	/** Mutable Instance Variables  */
+	private int points = 0;
+}
